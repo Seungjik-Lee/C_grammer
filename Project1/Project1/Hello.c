5e81457d000bf8e9fee8acdd03ad52c698e0b1e6
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-01-14 16:24:43 +0900

    Add files via upload

---
+++ Project1/Project1/Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 3;
+	int num2 = 4;
+	int result = num1 + num2;
+
+	printf("덧셈 결과 : %d \n%d+%d=%d \n%d와(과) %d의 합은 %d입니다.\n", result, num1, num2, result, num1, num2, result);
+
+	return 0;
+}
