e3b61386e2564849af4b2be602d88d81616b61ee
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-01-29 16:31:55 +0900

    Add files via upload

---
+++ 20210126/ex03.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int num = 8;
+    int *pnum;
+    pnum = &num;
+
+    printf("pnum value is \n");
+    printf("*pnum = %d \n", *pnum);
+    printf("pnum address value is \n");
+    printf("pnum = %d \n", pnum);
+
+    printf("&num = %d \n", &num);
+    
+}
