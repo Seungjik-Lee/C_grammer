a55eba860550e076d3644853f9e35babff76f39c
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-01-29 23:56:11 +0900

    Update chprob3.c

--- 20210128/chprob3.c
+++ 20210128/chprob3.c
@@ -2,14 +2,25 @@
 
 void main()
 {
-    int arr[10];
-    int i, num;
+	int arr[10];
+	int i, num;
+	int a = 0, b = 9;
 
-    printf("총 10개의 숫자 입력\n");
- 
-    for(i=0; i<10; i++)
-    {
-        printf("입력: ");
-        scanf("%d", &arr[i]);
-    }
+	printf("총 10개의 숫자 입력\n");
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("입력: ");
+		scanf("%d", &num);
+
+		if (num % 2 == 1)
+			arr[a++] = num;
+		else
+			arr[b--] = num;
+	}
+
+	printf("배열 요소의 출력 : ");
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+}
 }
