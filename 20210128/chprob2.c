efcc56ac9435d6439d8f11e2dd2f5198833143f0
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-02-01 15:06:10 +0900

    Update chprob2.c

--- 20210128/chprob2.c
+++ 20210128/chprob2.c
@@ -27,3 +27,40 @@ void main()
     }
 }
 
+/*
+void main()
+{
+    int arr[100];
+    int cnt = 0;
+    int input_number = 0;
+    int rema_number = 0;
+
+    printf("정수 입력 :");
+    scanf("%d", &input_number);
+
+    while(input_number > 1)
+    {
+        rema_number = input_number % 2;
+
+        arr[cnt] = rema_number;
+        cnt++;
+
+        int temp_value = input_number/2;
+        
+        if(temp_value<2)
+        {
+            arr[cnt] = temp_value;
+            cnt++;
+
+            break;
+        }
+        input_number = temp_value;
+    }
+
+    int i = 0;
+    for(i=cnt-1; i>-1; i--)
+    {
+        printf("%d", arr[i]);
+    }
+}
+*/
