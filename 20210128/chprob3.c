b3a8d53114f7b79877d6700f68bafc00c5d12ceb
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-02-01 15:07:02 +0900

    Update chprob3.c

--- 20210128/chprob3.c
+++ 20210128/chprob3.c
@@ -24,3 +24,42 @@ void main()
 		printf("%d ", arr[i]);
 }
 }
+
+/*
+void main()
+{
+    int arr[10];
+    int *fip = &arr[0];
+    int *eip = &arr[9];
+    int i = 0;
+
+    while(i<10)
+    {
+        int temp = 0;
+        printf("총 10개의 숫자 입력 : ");
+        scanf("%d", &temp);
+
+        //홀수
+        if(temp%2==1)
+        {
+            *fip = temp;
+            fip = fip+1;
+        }
+        
+        //짝수
+        else
+        {
+            *eip = temp;
+            eip = eip-1;
+        }
+        i++;
+    }
+
+    printf("\n");
+
+    for(i=0; i<10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+*/
