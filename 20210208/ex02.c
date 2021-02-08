e93c8930ab78f001424910cbb66ce760b0d11ef7
Seungjik-Lee <dltmdwlr@gmail.com>
2021-02-08 10:35:38 +0900

    ex02

--- 20210208/ex02.c
+++ 20210208/ex02.c
@@ -4,7 +4,14 @@ void main()
 {
     int a = 20;
     void *vp = &a;
+    int *ip;
 
     printf("&a = %d \n", &a);
     printf("vp = %d \n", vp);
+
+    *ip = (int*)vp;
+    *ip = 20;
+
+    printf("*ip = %d \n", *ip);
+    printf("ip = %d \n". ip)
 }
