569b23bb4b585a7f8e0eb04c587fb38a4daa098d
Seungjik-Lee <dltmdwlr@gmail.com>
2021-02-09 09:06:54 +0900

    도전프로그래밍3-1

---
+++ 20210208/chprob1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%-3d", arr[i][j]);
+        }
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[3 - j][i]);
+        }
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[3 - i][3 - j]);
+        }
+    }
+
+    printf("\n");
+    printf("\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[j][3 - i]);
+        }
+    }
+}
