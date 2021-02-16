e432cc90afaef0a79665292c0a824511c810e27f
Seungjik-Lee <dltmdwlr@gmail.com>
2021-02-16 11:57:18 +0900

    멤버 대 멤버 복사

---
+++ 20210216/ex05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int xpos;
+    int ypos;
+} Point;
+
+void main()
+{
+    Point p1 = {10, 20};
+    Point p2 = p1;
+
+    printf("p1.xpos = %d, p1.ypos = %d \n", p1.xpos, p1.ypos);
+    printf("p2.xpos = %d, p2.ypos = %d \n", p2.xpos, p2.ypos);
+
+    printf("\n");
+
+    p1.xpos = 30;
+
+    printf("\n");
+
+    printf("p1.xpos = %d, p1.ypos = %d \n", p1.xpos, p1.ypos);
+    printf("p2.xpos = %d, p2.ypos = %d \n", p2.xpos, p2.ypos);
+}
