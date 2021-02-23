685eb5895a16e284800aeed144ec7258fd18ed66
Seungjik-Lee <dltmdwlr@gmail.com>
2021-02-23 10:34:20 +0900

    힙영역

---
+++ 20210223/ex02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    //ptr1 -> 힙영역 주소값을 가르키고 있다.
+    int *ptr1 = (int *)malloc(sizeof(int));
+    int *ptr2 = (int *)malloc(sizeof(int) * 7);
+
+    *ptr1 = 20;
+
+    printf("*ptr1 = %d\n", *ptr1);
+
+    for (int i = 0; i < 7; i++)
+    {
+        *(ptr2 + i) = (i + 1);
+    }
+
+    printf("*ptr1 = %d\n", *ptr1);
+
+    for (int i = 0; i < 7; i++)
+    {
+        printf("*ptr2 = %d\n", *(ptr2 + i));
+    }
+
+    free(ptr1);
+    free(ptr2);
+}
