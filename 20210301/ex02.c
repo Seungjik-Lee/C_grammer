b62ab8b9b91709f9368685673dccce5e642a6836
dltmdwlr <dltmdwlr@gmail.com>
2021-03-02 09:43:23 +0900

    ex02

---
+++ 20210301/ex02.c
@@ -0,0 +1,12 @@
+#define STRING_JOB(A,B) #A"의 직업은 " #B"입니다."
+#define STNUM(Y, S, P) Y ## S ## P
+
+#include<stdio.h>
+
+void main()
+{
+    printf("%s \n", STRING_JOB(홍길동,나무꾼));
+    printf("%s \n", STRING_JOB(김길동,선녀));
+
+    printf("STNUM(10, 10, 10) = %d \n", STNUM(10, 10, 10));
+}
