4cb4b0b3d49e9868b62df9e815c756f124fa3b80
dltmdwlr <dltmdwlr@gmail.com>
2021-03-02 10:37:47 +0900

    mstruct

--- 20210301/cal.c
+++ 20210301/cal.c
@@ -1,4 +1,15 @@
+#ifndef __MSTRUCT_H__
+#define __MSTRUCT_H__
+#include "mstruct.h"
+#endif
+
 int c_square(int x)
 {
     return x*x;
+}
+
+void printMpoint(Mpoint point)
+{
+    printf("point x = ", point.x);
+    printf("point y = ", point.y);
 }
--- 20210301/cal.h
+++ 20210301/cal.h
@@ -1,1 +1,9 @@
+#ifndef __MSTRUCT_H__
+#define __MSTRUCT_H__
+#include "mstruct.h"
+#endif
+
 int c_square(int x);
+
+void printMpoint(Mpoint point);
+
